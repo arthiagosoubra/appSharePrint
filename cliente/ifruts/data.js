59f06efcf096e921a74387c9917f06c017a69d10
arthiagosoubra <158853696+arthiagosoubra@users.noreply.github.com>
2024-02-04 18:23:21 -0300

    Delete cliente/ifruts directory

--- cliente/ifruts/data.js
+++
@@ -1,9 +0,0 @@
-var data = 
-'CNPJ› 00.000.000/0000-00<br>'+
-'FONE› (83) 9.0000-0000<br>' +
-'END.› CEASA, Campina Grande - Bloco X, Box Y';
-
-var description = 'Serviço de Cupom Fiscal para administradores da loja iFruts.';
-var title = 'iFruts - ADM';
-
-var website = 'ifruts';
