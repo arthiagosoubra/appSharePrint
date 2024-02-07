98cb50f6b70b534e8155b0f85a7b52ae79adda0f
arthiagosoubra <158853696+arthiagosoubra@users.noreply.github.com>
2024-02-06 21:38:19 -0300

    Add files via upload

--- files/script/main.js
+++ files/script/main.js
@@ -35,6 +35,7 @@ $(document).ready(function () {
         }
     }
     $(metas).appendTo('head');
+    $('.picIt .logo').html(logo);
 
     toggleDarkMode();
     darkModeMediaQuery.addListener(toggleDarkMode);
