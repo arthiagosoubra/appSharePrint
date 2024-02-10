db34f6a86b598f24c28eb360a6dfc2717111ea9a
arthiagosoubra <158853696+arthiagosoubra@users.noreply.github.com>
2024-02-09 21:30:48 -0300

    Update check.js

--- files/check.js
+++ files/check.js
@@ -1,24 +1,3 @@
-document.getElementById('logo').addEventListener('click', function() {
-    if ('serviceWorker' in navigator && navigator.serviceWorker.controller) {
-        navigator.serviceWorker.getRegistration().then(function(registration) {
-            if (registration) {
-                registration.unregister().then(function() {
-                    caches.keys().then(function(cacheNames) {
-                        return Promise.all(
-                            cacheNames.map(function(cacheName) {
-                                return caches.delete(cacheName);
-                            })
-                        );
-                    }).then(function() {
-                        location.reload(true);
-                    });
-                });
-            }
-        });
-    }
-});
-
-
 $(document).ready(function () {
 
     $('body.hortifrutibrasileiros .warning.pay').hide(); //
